1654953feaf5562108f9e042bb9276e6b2394e22
Jorge Arley Ospina <joarospina@utp.edu.co>
2024-03-07 12:47:12 -0500

    app gifs react

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async(category) => {
+    const apiKey = '7k3aCH6u66UwNH5Q2Nb5ktIYQkCeFrvj';
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+    const resp = await fetch(url);
+
+    const { data } = await resp.json();
+
+    const gifs = data.map((gif)=> ({
+        id: gif.id,
+        title: gif.title,
+        url: gif.images.downsized_medium.url
+    }))
+
+    return gifs
+
+  }
